1b867ba111448906f960f9d23edfcf2a006dfe8b
ErnestAliev <ernestalievae@gmail.com>
2025-11-16 11:38:31 +0500

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -967,7 +967,7 @@ onBeforeUnmount(() => {
   flex-shrink: 0;
   height: 15px;
   width: 100%;
-  background-color: var(--color-background-soft);
+  background-color: #ffffff;
   border-bottom: 1px solid var(--color-border);
   position: relative;
 }
